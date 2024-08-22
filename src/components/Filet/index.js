872bb9a93ec7de3e4bf1e6294b5e99371f5e5694
OctavioCezarDaSilvaFaria <tavinhojabuca@gmail.com>
2024-08-22 11:46:18 -0300

    bugado

--- src/components/Filet/index.js
+++ src/components/Filet/index.js
@@ -2,35 +2,47 @@ import React from 'react';
 import "./style.css";
 import { Link } from "react-router-dom";
 
-export function Filet (){
-    return (
-        <div id="filet">
-      <div>
-        <Link to="/Futsal" className="link1">Futsal</Link>
-        </div>
-        <div>
-        <Link to="/Voleibol" className="link1">Voleibol</Link>
-        </div>
-        <div>
-        <Link to="/Handebol" className="link1">Handebol</Link>
-        </div>
-        <div>
-        <Link to="/Queimada" className="link1">Queimada</Link>
-        </div>
-        <div>
-        <Link to="/Basquete" className="link1">Basquete</Link>
-        </div>
-        <div>
-        <Link to="/Futmesa" className="link1">Futmesa</Link>
-        </div>
-        <div>
-        <Link to="/TennisDeMesa" className="link1">Tênis de Mesa</Link>
-        </div>
-        <div>
-        <Link to="/Atletismo" className="link1">Atletismo</Link>
-        </div>
+export function Filet() {
+   return (
+      <div id="filet">
+         <div className='filet-div-maior'>
+            <div className="filet-div">
+               <div>
+                  <Link to="/Futsal" className="link1">Futsal</Link>
+               </div>
+               <div>
+                  <Link to="/Voleibol" className="link1">Voleibol</Link>
+               </div>
+            </div>
+            <div className="filet-div">
+               <div>
+                  <Link to="/Handebol" className="link1">Handebol</Link>
+               </div>
+               <div>
+                  <Link to="/Queimada" className="link1">Queimada</Link>
+               </div>
+            </div>
+         </div>
+         <div className="filet-div-maior">
+            <div className="filet-div">
+               <div>
+                  <Link to="/Basquete" className="link1">Basquete</Link>
+               </div>
+               <div>
+                  <Link to="/Futmesa" className="link1">Futmesa</Link>
+               </div>
+            </div>
+            <div className="filet-div">
+               <div>
+                  <Link to="/TennisDeMesa" className="link1">Tênis de Mesa</Link>
+               </div>
+               <div>
+                  <Link to="/Atletismo" className="link1">Atletismo</Link>
+               </div>
+            </div>
+         </div>
       </div>
-    )
+   )
 }
 
 export default Filet;
